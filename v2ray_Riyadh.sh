a371e5cf33ad2097e2b3537bac813d5a9d9c1647
gaoqf <1435208021@qq.com>
2023-09-07 19:32:17 +0800

    添加沙特-利雅得脚本

---
+++ v2ray_Riyadh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 安装v2ray - 英国
+echo "1
+10
+31535
+
+" | bash <(curl -s -L https://raw.githubusercontent.com/Freedom-Money/v2ray-scripts/master/install.sh)
+
+# 修改时区端口
+echo "Asia/Riyadh
+
+" | bash <(curl -s -L https://raw.githubusercontent.com/Freedom-Money/v2ray-scripts/master/fm_custom.sh)
+
+# 显示v2ray二维码
+v2ray qr
